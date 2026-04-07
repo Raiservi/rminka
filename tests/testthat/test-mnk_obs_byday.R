48a9958856ffc24afedc37eb11c548a412a5f499
Raiservi <rai2525@hotmail.com>
2026-04-07 16:40:55 +0200

    .

---
+++ tests/testthat/test-mnk_obs_byday.R
@@ -0,0 +1,34 @@
+    # TEST 1
+    test_that("mnk_obs_byday handles small date ranges in one go", {
+      .mockPaths("mocks_byday/test1") # <--- Apunta a la nueva carpeta
+      with_mock_api({
+        obs <- mnk_obs_byday(d1 = "2024-05-20", d2 = "2024-05-20", quiet = TRUE)
+        expect_s3_class(obs, "tbl_df")
+        expect_equal(nrow(obs), 1)
+        expect_equal(obs$id, 101)
+      })
+      .mockPaths(NULL) # Resetea
+    })
+    #
+    # # TEST 2
+    # test_that("mnk_obs_byday subdivides requests when total results are large", {
+    #   .mockPaths("mocks_byday/test2") # <--- Apunta a la nueva carpeta
+    #   with_mock_api({
+    #     obs <- mnk_obs_byday(d1 = "2024-04-01", d2 = "2024-04-02", quiet = TRUE)
+    #     expect_s3_class(obs, "tbl_df")
+    #     expect_equal(nrow(obs), 2)
+    #     expect_equal(obs$id, c(201, 202))
+    #   })
+    #   .mockPaths(NULL) # Resetea
+    # })
+
+    # TEST 3
+    test_that("mnk_obs_byday downloads a full month as a single chunk", {
+      # Este test sigue usando la carpeta principal de mocks, donde están sus 100+ archivos
+      with_mock_api({
+        obs <- mnk_obs_byday(d1 = "2024-03-01", d2 = "2024-04-30", quiet = TRUE)
+        expect_s3_class(obs, "tbl_df")
+        expect_equal(nrow(obs), 2)
+        expect_equal(obs$id, c(301, 302))
+      })
+    })
