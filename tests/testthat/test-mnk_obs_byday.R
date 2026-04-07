2b1f2a1b7723301fcf774c8e2355644fc2ae0540
projectehippocampusbcn <projectehippocampusbcn@gmail.com>
2026-04-07 20:06:13 +0200

    .

--- tests/testthat/test-mnk_obs_byday.R
+++ tests/testthat/test-mnk_obs_byday.R
@@ -1,13 +1,12 @@
     # TEST 1
     test_that("mnk_obs_byday handles small date ranges in one go", {
-      .mockPaths("mocks_byday/test1") # <--- Apunta a la nueva carpeta
-      with_mock_api({
+      httptest::with_mock_api({
         obs <- mnk_obs_byday(d1 = "2024-05-20", d2 = "2024-05-20", quiet = TRUE)
         expect_s3_class(obs, "tbl_df")
         expect_equal(nrow(obs), 1)
         expect_equal(obs$id, 101)
       })
-      .mockPaths(NULL) # Resetea
+
     })
     #
     # # TEST 2
@@ -25,7 +24,7 @@
     # TEST 3
     test_that("mnk_obs_byday downloads a full month as a single chunk", {
       # Este test sigue usando la carpeta principal de mocks, donde están sus 100+ archivos
-      with_mock_api({
+      httptest::with_mock_api({
         obs <- mnk_obs_byday(d1 = "2024-03-01", d2 = "2024-04-30", quiet = TRUE)
         expect_s3_class(obs, "tbl_df")
         expect_equal(nrow(obs), 2)
