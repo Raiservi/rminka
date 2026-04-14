cf84ae55a86bf105f0fb7e415ebfe8f7cdb3f863
Raiservi <rai2525@hotmail.com>
2026-04-14 08:35:21 +0200

    .

--- R/utils.R
+++ R/utils.R
@@ -13,19 +13,7 @@ utils::globalVariables(c(
   "header_image_url"         # Columna 'header_image_url' del proyecto
 ))
 
-#' Pipe operator
-#'
-#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
-#'
-#' @name %>%
-#' @rdname pipe
-#' @keywords internal
-#' @export
 #' @importFrom magrittr %>%
-#' @usage lhs \%>\% rhs
-#' @param lhs A value or the magrittr placeholder.
-#' @param rhs A function call using the magrittr semantics.
-#' @return The result of calling `rhs(lhs)`.
 NULL
 
 #' @importFrom rlang %||%
